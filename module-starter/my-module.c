16030946886e3e594a09f2c94992fd4b7aeb0f6a
Michael Harris <46806666+MichaelHarrisCode@users.noreply.github.com>
2025-03-06 21:48:11 -0800

    fix module-starter checkpath errors

--- module-starter/my-module.c
+++ module-starter/my-module.c
@@ -1,16 +1,18 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
 
 static int __init my_module_init(void)
 {
-	printk(KERN_INFO "Hello, World!\n");
+	pr_info("Hello, World!\n");
 	return 0;
 }
 
 static void __exit my_module_exit(void)
 {
-	printk(KERN_INFO "Goodbye, World!\n");
+	pr_info("Goodbye, World!\n");
 }
 
 module_init(my_module_init);
